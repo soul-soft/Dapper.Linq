09240ae735e0a659668f725d29e6d76161417c07
1448376744 <41883220+1448376744@users.noreply.github.com>
2019-06-28 10:21:35 +0800

    11

---
+++ Src/Dapper.Common/Dapper.Common/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper.Common
+{
+    public class Logger
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+        public long ExecuteTime { get; set; }
+        public int? Timeout { get; set; }
+        public bool? Buffered { get; set; }
+    }
+}
