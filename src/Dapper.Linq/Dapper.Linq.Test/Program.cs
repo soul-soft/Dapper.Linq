6a4bf3ac67ca7b4be74df1cd26c1f8b585745fe0
王剑锋 <1448376744@qq.com>
2023-11-13 17:09:16 +0800

    dev

--- src/Dapper.Linq/Dapper.Linq.Test/Program.cs
+++ src/Dapper.Linq/Dapper.Linq.Test/Program.cs
@@ -6,5 +6,7 @@ Console.WriteLine("Hello, World!");
 int s = 20;
 var context = new DbContext(null);
 var sql = context.From<Student>("pro")
-    .Where(a=>a.Id == s)
-  .Sum(a => a.Id);
+    .Set(a => a.Name, "ff")
+    .Where(a => a.Id == s)
+    .Update();
+
